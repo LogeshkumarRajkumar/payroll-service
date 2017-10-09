6abd7bada9f6f9cf8982052b1285434a03962442
Logeshkumar <Logeshkr@thoughtworks.com>
2017-10-09 19:23:30 +0530

    allow host

--- payrollservice/settings.py
+++ payrollservice/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 STATIC_URL = '/static/'
