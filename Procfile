60a3479e40a8ee8ff57d1bc96b7f6b98873cbc2e
logeshkumar <logeshkr@thoughtworks.com>
2018-04-07 16:04:19 +0530

    made service to point postgres database

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: python manage.py makemigrations
 release: python manage.py migrate
 web: gunicorn payrollservice.wsgi --log-file -
--- payrollservice/settings.py
+++ payrollservice/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import dj_database_url
 import os
 import datetime
 
@@ -105,10 +106,13 @@ WSGI_APPLICATION = 'payrollservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
