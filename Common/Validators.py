1df613cd56178e568c79b42ab545367cf30033d6
logeshkumar <logeshkr@thoughtworks.com>
2018-05-27 14:05:56 +0530

    Adds validations for registrations

---
+++ Common/Validators.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ValidationError
+import re
+
+
+def validate_domainonly_email(value):
+    """
+    Let's validate the email passed is in the domain "yourdomain.com"
+    """
+    if not "yourdomain.com" in value:
+        raise ValidationError(
+            "Sorry, the email submitted is invalid. All emails have to be registered on this domain only.")
+
+
+def validate_alpha_numeric(value):
+    """
+    Let's validate the email passed is in the domain "yourdomain.com"
+    """
+    if not re.match(r'^[0-9a-zA-Z]*$', value):
+        raise ValidationError(
+            "Sorry, Input should contain only [a-z][A-Z][0-9] (alphanumeric characters) only.")
--- Users/models.py
+++ Users/models.py
@@ -7,13 +7,15 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 from .managers import UserManager
+from Common.Validators import validate_alpha_numeric
 import uuid
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.CharField(max_length=20000, primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=20000, blank=True)
-    last_name = models.CharField(_('last name'), max_length=20000, blank=True)
+    first_name = models.CharField(_('first name'), max_length=20000, validators=[validate_alpha_numeric], blank=False)
+    last_name = models.CharField(_('last name'), max_length=20000, validators=[validate_alpha_numeric], blank=False)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
     jwt_secret = models.UUIDField(default=uuid.uuid4)
@@ -28,23 +30,24 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('users')
 
     def get_full_name(self):
-        '''
+        """
         Returns the first_name plus the last_name, with a space in between.
-        '''
+        """
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
-        '''
+        """
         Returns the short name for the user.
-        '''
+        """
         return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
+        """
         Sends an email to this User.
-        '''
+        """
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
 
 def jwt_get_secret_key(user_model):
     return user_model.jwt_secret
